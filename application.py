e078233a4da0694887dd34eb1efaeb2554e951e3
Jafer <itsjafer@gmail.com>
2020-09-08 05:50:20 -0400

    Initial commit of flask parser backend

---
+++ application.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import jsonify
+import json
+import requests
+from flask_cors import CORS
+from flask_cors import cross_origin
+from flask import request
+
+
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+CORS(application)
+
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
+
+
+@application.route('/parse', methods=['POST'])
+def parse_resume():
+    print(request.form)
+    resume = request.files['file']
+    response = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=resume))
+
+    return response.json()
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
